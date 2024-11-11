c99c6c80650b7c08be9de888231573c01675f9b7
clairebae03 <92658164+clairebae03@users.noreply.github.com>
2024-11-11 11:25:58 -0500

    working on reports

--- frontend/src/components/Reports.js
+++ frontend/src/components/Reports.js
@@ -1,7 +1,12 @@
 import React from "react";
+import './Reports.css';
 
 const Reports = () => {
-  return <div>still building Weekly Reports...</div>;
+  return(
+    <div className="get-reports">
+      <h1>View the Week's Progress</h1>
+    </div>
+  )
 };
 
 export default Reports;
