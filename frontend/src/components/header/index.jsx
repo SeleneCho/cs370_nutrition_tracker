7edbb0f3f0e6b6b0c41e838476153cbc259b9dc3
clairebae03 <92658164+clairebae03@users.noreply.github.com>
2024-12-09 04:01:53 -0500

    fix logout button design

--- frontend/src/components/header/index.jsx
+++ frontend/src/components/header/index.jsx
@@ -2,17 +2,28 @@ import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/authContext'
 import { doSignOut } from '../../firebase/auth'
+import "./logout.css";
+
 
 const Header = () => {
     const navigate = useNavigate()
     const { userLoggedIn } = useAuth()
     return (
-        <nav className='flex flex-row gap-x-2 w-full z-20 fixed top-0 left-0 h-12 border-b place-content-center items-center bg-gray-200'>
+        <nav className='logout'>
             {
                 userLoggedIn
                     ?
                     <>
-                        <button onClick={() => { doSignOut().then(() => { navigate('/login') }) }} className='text-sm text-blue-600 underline'>Logout</button>
+                        <button
+                            onClick={() => {
+                                doSignOut().then(() => {
+                                navigate('/login');
+                                });
+                            }}
+                            className="logout-button"
+                        >
+                            Logout
+                        </button>
                     </>
                     :
                     <>
